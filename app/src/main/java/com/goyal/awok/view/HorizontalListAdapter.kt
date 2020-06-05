723ed5e9b3a7b9ded95090d4e6e5e6d1571301a5
Sakshi Agarwal <devsakshiagarwal@gmail.com>
2020-06-05 17:39:36 +0400

    added adapters

---
+++ app/src/main/java/com/goyal/awok/view/HorizontalListAdapter.kt
@@ -0,0 +1,70 @@
+package com.goyal.awok.view
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.goyal.awok.R
+import com.goyal.awok.model.schema.Item
+import kotlinx.android.synthetic.main.item_product_horizontal.view.btn_add_to_cart
+import kotlinx.android.synthetic.main.item_product_horizontal.view.iv_product
+import kotlinx.android.synthetic.main.item_product_horizontal.view.tv_offer
+import kotlinx.android.synthetic.main.item_product_horizontal.view.tv_new_price
+import kotlinx.android.synthetic.main.item_product_horizontal.view.tv_old_price
+import kotlinx.android.synthetic.main.item_product_horizontal.view.tv_product_name
+
+class HorizontalListAdapter(
+  private val list: List<Item>,
+  private val context: Context
+) :
+    RecyclerView.Adapter<HorizontalListAdapter.HorizontalItemViewHolder>() {
+
+  override fun onCreateViewHolder(
+    parent: ViewGroup,
+    viewType: Int
+  ): HorizontalItemViewHolder {
+    return HorizontalItemViewHolder(
+        LayoutInflater.from(context)
+            .inflate(R.layout.item_product_horizontal, parent, false)
+    )
+  }
+
+  override fun onBindViewHolder(
+    holder: HorizontalItemViewHolder,
+    position: Int
+  ) {
+    val item: Item = list[position]
+    holder.tvProductName.text = item.name
+    holder.tvOldPrice.text = item.prices.oldPrice
+    holder.tvNewPrice.text = item.prices.newPrice
+    holder.tvOffer.text = item.prices.discount
+    Glide.with(context)
+        .load(item.image.imageSource)
+        .centerCrop()
+        .placeholder(R.drawable.ic_place_holder)
+        .error(R.drawable.ic_place_holder)
+        .fallback(R.drawable.ic_place_holder)
+        .into(holder.ivProduct)
+    holder.btnAddToCart.setOnClickListener {
+      Toast.makeText(context, item.name + "added to cart", Toast.LENGTH_SHORT)
+          .show()
+    }
+  }
+
+  override fun getItemCount(): Int = list.size
+
+  class HorizontalItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val tvProductName: AppCompatTextView = view.tv_product_name
+    val tvOldPrice: AppCompatTextView = view.tv_old_price
+    val tvNewPrice: AppCompatTextView = view.tv_new_price
+    val tvOffer: AppCompatTextView = view.tv_offer
+    val ivProduct: AppCompatImageView = view.iv_product
+    val btnAddToCart: AppCompatButton = view.btn_add_to_cart
+  }
+}
---
+++ app/src/main/java/com/goyal/awok/view/VerticalListAdapter.kt
@@ -0,0 +1,67 @@
+package com.goyal.awok.view
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.goyal.awok.R
+import com.goyal.awok.model.schema.Item
+import com.goyal.awok.view.VerticalListAdapter.VerticalItemViewHolder
+import kotlinx.android.synthetic.main.item_product_horizontal.view.tv_offer
+import kotlinx.android.synthetic.main.item_product_vertical.view.*
+
+class VerticalListAdapter(
+  private val list: List<Item>,
+  private val context: Context
+) :
+    RecyclerView.Adapter<VerticalItemViewHolder>() {
+
+  override fun onCreateViewHolder(
+    parent: ViewGroup,
+    viewType: Int
+  ): VerticalItemViewHolder {
+    return VerticalItemViewHolder(
+        LayoutInflater.from(context)
+            .inflate(R.layout.item_product_vertical, parent, false)
+    )
+  }
+
+  override fun onBindViewHolder(
+    holder: VerticalItemViewHolder,
+    position: Int
+  ) {
+    val item: Item = list[position]
+    holder.tvProductName.text = item.name
+    holder.tvOldPrice.text = item.prices.oldPrice
+    holder.tvNewPrice.text = item.prices.newPrice
+    holder.tvOffer.text = item.prices.discount
+    Glide.with(context)
+        .load(item.image.imageSource)
+        .centerCrop()
+        .placeholder(R.drawable.ic_place_holder)
+        .error(R.drawable.ic_place_holder)
+        .fallback(R.drawable.ic_place_holder)
+        .into(holder.ivProduct)
+    holder.btnAddToCart.setOnClickListener {
+      Toast.makeText(context, item.name + "added to cart", Toast.LENGTH_SHORT)
+          .show()
+    }
+  }
+
+  override fun getItemCount(): Int = list.size
+
+  class VerticalItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val tvProductName: AppCompatTextView = view.tv_product_name
+    val tvOldPrice: AppCompatTextView = view.tv_old_price
+    val tvNewPrice: AppCompatTextView = view.tv_new_price
+    val tvOffer: AppCompatTextView = view.tv_offer
+    val ivProduct: AppCompatImageView = view.iv_product_plp
+    val btnAddToCart: AppCompatButton = view.btn_add_to_cart
+  }
+}
